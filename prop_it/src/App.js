16f2a1cc057aa89d450a1aa84600fb9dc4b9590c
Carter Ireland <carter.n.ireland@gmail.com>
2021-05-06 18:18:38 -0700

    My assignment

---
+++ prop_it/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import MyComponent from './components/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent firstName='Carter' lastName='Ireland' age='19' hairColor='Brown'/>
+      <MyComponent firstName='Addy' lastName='Ireland' age='17' hairColor='Brown'/>
+      <MyComponent firstName='James' lastName='Ireland' age='?' hairColor='Brown'/>
+      <MyComponent firstName='Michelle' lastName='Cargill' age='?' hairColor='Brown'/>
+    </div>
+  );
+}
+
+export default App;
