8d1bd6ceb3b2194dff83995cc68d4d00e41905f0
Carter Ireland <carter.n.ireland@gmail.com>
2021-05-07 16:02:30 -0700

    Assignment

---
+++ prop_it_2/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import PersonCard from './components/PersonCard.js';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName='Carter' lastName='Ireland' age={19} hairColor='Brown'/>
+      <PersonCard firstName='Addy' lastName='Ireland' age={17} hairColor='Brown'/>
+      <PersonCard firstName='James' lastName='Ireland' age={49} hairColor='Brown'/>
+      <PersonCard firstName='Michelle' lastName='Cargill' age={52} hairColor='Brown'/>
+    </div>
+  );
+}
+
+export default App;
