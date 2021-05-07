f8bf71b24951b8ad19c1042c7a9736faf9bd2c35
Carter Ireland <carter.n.ireland@gmail.com>
2021-05-06 19:05:02 -0700

    Prop it continued

--- prop_it/src/App.js
+++ prop_it/src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+
   return (
     <div className="App">
       <MyComponent firstName='Carter' lastName='Ireland' age='19' hairColor='Brown'/>
       <MyComponent firstName='Addy' lastName='Ireland' age='17' hairColor='Brown'/>
-      <MyComponent firstName='James' lastName='Ireland' age='?' hairColor='Brown'/>
-      <MyComponent firstName='Michelle' lastName='Cargill' age='?' hairColor='Brown'/>
+      <MyComponent firstName='James' lastName='Ireland' age='49' hairColor='Brown'/>
+      <MyComponent firstName='Michelle' lastName='Cargill' age='52' hairColor='Brown'/>
     </div>
   );
 }
--- prop_it/src/components/MyComponent.js
+++ prop_it/src/components/MyComponent.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 
 class MyComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age : props.age
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.firstName} {this.props.lastName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={ () => {this.setState({age: (parseInt(this.state.age) + 1)})}}>Add a year</button>
             </div>
         );
     }
